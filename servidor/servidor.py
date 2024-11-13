54c8ada5c9a29d8496deeef9ad823a46a84677f0
HenriqueMartinelliPinheiro <48884506+HenriqueMartinelliPinheiro@users.noreply.github.com>
2024-11-13 20:42:41 -0300

    envio para a fila de saida

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -3,8 +3,8 @@ import threading
 import time
 import json
 
-NUM_FILAS_RESERVA = 2  # Defina o número de filas de reserva
-NUM_FILAS_PROCESSAMENTO = 2  # Defina o número de filas de processamento
+NUM_FILAS_RESERVA = 2  # Número de filas de reserva
+NUM_FILAS_PROCESSAMENTO = 2  # Número de filas de processamento
 
 
 class Ingresso:
@@ -18,40 +18,42 @@ class Ingresso:
         if self.usuario_reserva_id is None:
             self.usuario_reserva_id = usuario_id
             print(
-                f"[Servidor - Reserva] Ingresso {self.ingresso_id} reservado pelo usuário {usuario_id}.")
+                f"[Servidor - Reserva] Ingresso {self.ingresso_id} reservado pelo usuário {usuario_id}.", flush=True)
             return True
         print(
-            f"[Servidor - Reserva] Ingresso {self.ingresso_id} já foi reservado pelo usuário {self.usuario_reserva_id}.")
+            f"[Servidor - Reserva] Ingresso {self.ingresso_id} já foi reservado pelo usuário {self.usuario_reserva_id}.", flush=True)
         return False
 
     def vender(self, usuario_id):
         if self.usuario_reserva_id == usuario_id:
             self.usuario_comprador_id = usuario_id
             print(
-                f"[Servidor - Venda] Ingresso {self.ingresso_id} comprado pelo usuário {usuario_id}.")
+                f"[Servidor - Venda] Ingresso {self.ingresso_id} comprado pelo usuário {usuario_id}.", flush=True)
             return True
         print(
-            f"[Servidor - Venda] Ingresso {self.ingresso_id} não pode ser vendido ao usuário {usuario_id}.")
+            f"[Servidor - Venda] Ingresso {self.ingresso_id} não pode ser vendido ao usuário {usuario_id}.", flush=True)
         return False
 
 
 class ServidorIngressos:
     def __init__(self):
-        self.contador_ingressos_disponiveis = 500
+        self.contador_ingressos_disponiveis = 50
         self.contador_ingressos_reservados = 0
         self.contador_lock = threading.Lock()
         self.ingressos_reservados = {}
         self.locks = {}
+        self.reservas_paradas = False
 
     def criar_conexao(self):
         while True:
             try:
                 connection = pika.BlockingConnection(
-                    pika.ConnectionParameters('rabbitmq', 5672))
-                print("[Servidor] Conexão estabelecida com o RabbitMQ.")
+                    pika.ConnectionParameters('rabbitmq', 5672)
+                )
+                print("[Servidor] Conexão estabelecida com o RabbitMQ.", flush=True)
                 return connection
             except pika.exceptions.AMQPConnectionError as e:
-                print("[Servidor] Erro ao conectar com o RabbitMQ:", e)
+                print("[Servidor] Erro ao conectar com o RabbitMQ:", e, flush=True)
                 time.sleep(5)
 
     def get_lock(self, ingresso_id):
@@ -63,16 +65,21 @@ class ServidorIngressos:
         connection = self.criar_conexao()
         channel = connection.channel()
         channel.queue_declare(queue=f'FilaReserva{fila_id}', durable=True)
-        thread_id = threading.get_ident()
 
         while True:
+            with self.contador_lock:
+                # Se todos os ingressos estão reservados, interrompe as reservas e começa a monitorar
+                if self.contador_ingressos_disponiveis == 0 and self.contador_ingressos_reservados > 0:
+                    self.reservas_paradas = True
+                    break
+
             method_frame, header_frame, body = channel.basic_get(
                 queue=f'FilaReserva{fila_id}', auto_ack=True)
             if body:
                 ingresso_data = body.decode().split(',')
                 ingresso_id, evento_id, usuario_id = map(int, ingresso_data)
-
                 lock = self.get_lock(ingresso_id)
+
                 with lock:
                     if ingresso_id not in self.ingressos_reservados:
                         ingresso = Ingresso(ingresso_id, evento_id)
@@ -82,30 +89,82 @@ class ServidorIngressos:
                                 self.contador_ingressos_disponiveis -= 1
                                 self.contador_ingressos_reservados += 1
                             print(
-                                f"[Servidor - Reserva] FilaReserva{fila_id} - Thread {thread_id}: Ingresso {ingresso_id} reservado e enviado para processamento.")
+                                f"[Servidor - Reserva] FilaReserva{fila_id}: Ingresso {ingresso_id} reservado.", flush=True)
 
-                fila_processamento_menos_ocupada = None
-                menor_tamanho = float('inf')
-                for i in range(1, NUM_FILAS_PROCESSAMENTO + 1):
-                    queue = channel.queue_declare(
-                        queue=f'FilaProcessamento{i}', durable=True, passive=True)
-                    if queue.method.message_count < menor_tamanho:
-                        menor_tamanho = queue.method.message_count
-                        fila_processamento_menos_ocupada = f'FilaProcessamento{i}'
+                            fila_processamento_menos_ocupada = self.obter_fila_processamento_menos_ocupada(
+                                channel)
+                            if fila_processamento_menos_ocupada:
+                                channel.basic_publish(
+                                    exchange='', routing_key=fila_processamento_menos_ocupada, body=body)
+                                print(
+                                    f"[Servidor - Reserva] FilaReserva{fila_id}: Ingresso {ingresso_id} enviado para {fila_processamento_menos_ocupada}", flush=True)
 
-                if fila_processamento_menos_ocupada:
-                    channel.basic_publish(
-                        exchange='', routing_key=fila_processamento_menos_ocupada, body=body)
+            time.sleep(2)
+
+        # Entra no loop de verificação até liberar novas reservas ou ingressos esgotarem
+        self.verificar_ingressos(channel, fila_id)
+
+    def verificar_ingressos(self, channel, fila_id):
+        while True:
+            with self.contador_lock:
+                # Se todos os ingressos foram vendidos, notifica "esgotado" e move requisições pendentes
+                if self.contador_ingressos_disponiveis == 0 and self.contador_ingressos_reservados == 0:
+                    self.enviar_mensagem_esgotado(channel)
+                    self.mover_requisicoes_pendentes_para_fila_saida(
+                        channel, fila_id)
+                    break
+                # Se há ingressos disponíveis novamente, retoma as reservas
+                elif self.contador_ingressos_disponiveis > 0:
+                    self.reservas_paradas = False
                     print(
-                        f"[Servidor - Reserva] FilaReserva{fila_id} - Thread {thread_id}: Ingresso {ingresso_id} enviado para {fila_processamento_menos_ocupada}")
-            time.sleep(1)
+                        f"[Servidor - Reserva] FilaReserva{fila_id}: Reservas retomadas.", flush=True)
+                    break
+
+            print(
+                f"[Servidor - Reserva] FilaReserva{fila_id}: Aguardando disponibilidade de ingressos...", flush=True)
+            time.sleep(5)
+
+    def enviar_mensagem_esgotado(self, channel):
+        mensagem = {"status": "Esgotado",
+                    "mensagem": "Todos os ingressos foram vendidos."}
+        channel.basic_publish(
+            exchange='', routing_key='FilaSaida', body=json.dumps(mensagem))
+        print("[Servidor - Reserva] Ingressos esgotados. Mensagem de esgotamento enviada para a fila de saída.", flush=True)
+
+    def mover_requisicoes_pendentes_para_fila_saida(self, channel, fila_id):
+        print(
+            f"[Servidor - Reserva] FilaReserva{fila_id}: Movendo requisições pendentes para FilaSaida.", flush=True)
+        while True:
+            method_frame, header_frame, body = channel.basic_get(
+                queue=f'FilaReserva{fila_id}', auto_ack=True)
+            if not body:
+                break  # Sai do loop quando não houver mais requisições pendentes
+
+            mensagem = {
+                "status": "Esgotado",
+                "mensagem": "Ingressos esgotados - requisição não processada."
+            }
+            channel.basic_publish(
+                exchange='', routing_key='FilaSaida', body=json.dumps(mensagem))
+            print(
+                f"[Servidor - Reserva] FilaReserva{fila_id}: Requisição movida para FilaSaida devido ao esgotamento.", flush=True)
+
+    def obter_fila_processamento_menos_ocupada(self, channel):
+        menor_tamanho = float('inf')
+        fila_processamento_menos_ocupada = None
+        for i in range(1, NUM_FILAS_PROCESSAMENTO + 1):
+            queue = channel.queue_declare(
+                queue=f'FilaProcessamento{i}', durable=True, passive=True)
+            if queue.method.message_count < menor_tamanho:
+                menor_tamanho = queue.method.message_count
+                fila_processamento_menos_ocupada = f'FilaProcessamento{i}'
+        return fila_processamento_menos_ocupada
 
     def consumir_fila_processamento(self, fila_id):
         connection = self.criar_conexao()
         channel = connection.channel()
         channel.queue_declare(
             queue=f'FilaProcessamento{fila_id}', durable=True)
-        thread_id = threading.get_ident()
 
         while True:
             method_frame, header_frame, body = channel.basic_get(
@@ -122,7 +181,7 @@ class ServidorIngressos:
                             self.contador_ingressos_reservados -= 1
                             if self.contador_ingressos_reservados == 0 and self.contador_ingressos_disponiveis == 0:
                                 print(
-                                    "[Servidor - Processamento] Todos os ingressos foram vendidos e finalizados.")
+                                    "[Servidor - Processamento] Todos os ingressos foram vendidos e finalizados.", flush=True)
 
                         resposta = {
                             "ingresso_id": ingresso_id,
@@ -131,14 +190,12 @@ class ServidorIngressos:
                             "usuario_comprador_id": ingresso.usuario_comprador_id,
                             "status": "vendido"
                         }
-                        print(
-                            f"[Servidor - Processamento] FilaProcessamento{fila_id} - Thread {thread_id}: Ingresso {ingresso_id} vendido e finalizado.")
                         channel.basic_publish(
                             exchange='', routing_key='FilaSaida', body=json.dumps(resposta))
-                    else:
                         print(
-                            f"[Servidor - Processamento] FilaProcessamento{fila_id} - Thread {thread_id}: Ingresso {ingresso_id} não pode ser vendido ao usuário {usuario_id}.")
-            time.sleep(1)
+                            f"[Servidor - Processamento] FilaProcessamento{fila_id}: Ingresso {ingresso_id} vendido.", flush=True)
+
+            time.sleep(3)
 
 
 if __name__ == "__main__":
